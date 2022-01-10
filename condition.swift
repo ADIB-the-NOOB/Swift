d6b40a4301feb22baafb32882c695ab106c5fde1
ADIB-the-NOOB <mdadib550@gmail.com>
2022-01-10 19:40:26 +0600

    Condition 01

--- condition.swift
+++ condition.swift
@@ -1,10 +1,56 @@
-let a = 23
-if a > 2{
+let v = 23
+if v > 2{
     print("a is greater than 2")
 }
-else if a != 2 {
+else if v != 2 {
     print("a is not equal to 2")
 }
 else {
     print("a is equal to 2")    
+}
+
+print()
+print()
+print()
+//another example
+
+let a = 23
+let b = 4
+let c = 34
+
+if a > b && c == 34 {
+    print("a is greater than b and c is equal to 34")
+}
+else if a > b || c == 34 {
+    print("a is greater than b or c is equal to 34")
+}
+else {
+    print("a is less than b and c is not equal to 34")
+}
+
+//another example01
+
+let adib = 23
+let sadib = 34
+let ajib = 45
+
+if (adib < 23 || sadib > 12){
+    print("adib is less than 23 or sadib is greater than 12")
+    
+}
+
+
+//another example03
+
+var x = 34
+var y = 45
+
+if (x == y){
+    print("x is equal to y")    
+}
+
+//another example04
+
+if !(x == y){
+    print("Not True Checking....")    
 }
