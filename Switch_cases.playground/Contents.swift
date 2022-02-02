eda6e0f4f2b0034e97a8ad4c9b904aea03b50c4e
ADIB-the_NOOB <mdadib550@gmail.com>
2022-02-02 14:31:08 +0600

    SwitchCase - Primary

---
+++ Switch_cases.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var someCharacter:Character = "c"
+
+switch someCharacter {
+    case "a":
+        print("is an A")
+    case "b":
+       print("is a B")
+    default:
+        print("some Fallback")
+}
