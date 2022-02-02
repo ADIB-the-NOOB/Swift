bf57cb65a0a37c9e786abff10c968e76de78239e
ADIB <adib@iMac-ADIB.local>
2022-02-02 13:51:36 +0600

    Rounded Figure of Int

---
+++ Data_types.playground/Contents.swift
@@ -0,0 +1,18 @@
+var c:Float = 2.3
+var d:Double = 13.9
+var e:Bool = true
+
+//Using Int for C
+print(Int(c))
+
+// Using Int for d
+print(Int(d))
+
+//printing the type
+print(type(of: e))
+
+// Round Figure of any Float Num....
+var float: Float = 3.42
+float.round()
+
+
